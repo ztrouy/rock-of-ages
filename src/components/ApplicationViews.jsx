09f2b3a023b8557660225d9d8dadf63be69fdfb5
Zachary Trouy <34666901+ztrouy@users.noreply.github.com>
2024-07-26 21:57:39 -0500

    Updated routes for All Rocks and My Rocks views to use new bespoke components

--- src/components/ApplicationViews.jsx
+++ src/components/ApplicationViews.jsx
@@ -4,8 +4,10 @@ import { Authorized } from "./Authorized"
 import { Login } from "../pages/Login.jsx"
 import Home from "../pages/Home"
 import { RockForm } from "./RockForm.jsx"
-import { RockList } from "./RockList.jsx"
+import RockList from "./RockList.jsx"
 import { Register } from '../pages/Register.jsx'
+import AllRocks from '../pages/AllRocks.jsx'
+import MyRocks from '../pages/MyRocks.jsx'
 
 
 export const ApplicationViews = () => {
@@ -40,9 +42,9 @@ export const ApplicationViews = () => {
             <Route path="/register" element={<Register />} />
             <Route element={<Authorized />}>
                 <Route path="/" element={<Home />} />
-                <Route path="/allrocks" element={<RockList rocks={rocksState} fetchRocks={fetchRocksFromAPI} />} />
+                <Route path="/allrocks" element={<AllRocks rocks={rocksState} fetchRocks={fetchRocksFromAPI} />} />
                 <Route path="/create" element={<RockForm fetchRocks={fetchRocksFromAPI} />} />
-                <Route path="/mine" element={<RockList rocks={rocksState} fetchRocks={fetchRocksFromAPI} />} />
+                <Route path="/mine" element={<MyRocks rocks={rocksState} fetchRocks={fetchRocksFromAPI} />} />
             </Route>
         </Routes>
     </BrowserRouter>
